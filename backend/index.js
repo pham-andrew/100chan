3930f69fd3f59168ff27df045f8735927a8e13b9
pham-andrew <andrewpham@g.harvard.edu>
2021-07-20 08:06:47 -0700

    initialized

---
+++ backend/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+});
+
+app.get("/api", (req, res) => {
+    res.json({ message: "Hello from server!" });
+  });
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
---
+++ frontend/src/App.js
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from 'react';
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  const [data, setData] = React.useState(null);
+
+  useEffect(() => {
+    async function f(){
+      console.log("running")
+      await fetch("http://localhost:3001/api")
+        .then((res) => res.json())
+        .then((data) => setData(data.message));
+      }
+    f()
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>{data}</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
