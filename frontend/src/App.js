fef6780a0bb6f16a00129718f4cb0158ce6ba029
pham-andrew <andrewpham@g.harvard.edu>
2021-07-20 14:24:29 -0700

    name change

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -172,7 +172,7 @@ export default function App() {
             <MenuIcon />
           </IconButton>
           <Typography component="h1" variant="h6" color="inherit" noWrap className={classes.title}>
-            100chan
+            4ch
           </Typography>
           <IconButton color="inherit" onClick={handleDialogOpen}>
             <Badge color="secondary">
